a388f50bd3e7dd7cffd7f433794aec92acd10cec
Chase Thomas <cj.thomas19@students.bchigh.edu>
2020-07-16 16:25:38 -0400

    Added Example 1, Added a comment in Project 1

---
+++ Ex1_Variable_Types.java
@@ -0,0 +1,28 @@
+public class Ex1_Variable_Types {
+
+    public static void main(String[] args) {
+
+        //Integers are representations of whole numbers (without a decimal place).
+        int i_1 = 5;
+        int i_2 = 3;
+
+        //Doubles and floats are used for numbers with decimal points.
+        //Doubles are more precise and are mostly what we will use
+        double d_1 = 5.66;
+        double d_2 = 7.12;
+
+        //Characters represent individual letters and symbols
+        char c_1 = 'a';
+        char c_2 = '1';
+
+        //Booleans represent either logical true or false values
+        boolean b_1 = true;
+        boolean b_2 = false;
+
+        //These are all primitive data types, as evidenced by the lowercase in the type delaration
+        //Strings represent words or phrases. They are not primitives and are a class type.
+        String s_1 = "Hello, World!";
+        
+    }
+    
+}
--- P1_Hello_World.java
+++ P1_Hello_World.java
@@ -1,8 +1,10 @@
 public class P1_Hello_World {
 
+    //This is our first program!
     public static void main( String[] args) {
-        
+
         System.out.println("Hello World");
+
     }
 
 }
