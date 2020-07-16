ed2fcc2ecca29d6da804f235a7d177033f02340b
Chase Thomas <cj.thomas19@students.bchigh.edu>
2020-07-16 17:26:20 -0400

    New Examples

---
+++ P2_Numbers.java
@@ -0,0 +1,16 @@
+public class P2_Numbers {
+    
+    public static void main(String[] args) {
+
+        //Here, we declare our integers and assign their values
+        int a = 4;
+        int b = 5;
+
+        //Try changing the '*' (times) to another operator (+, -, or /) and see what happens:
+        System.out.println(a * b);
+
+        //Here, we declare and assign a new variable based on the values of a and b
+        int c = a + b;
+        
+    }
+}
