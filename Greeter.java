9981fda18a3542242e07a08bce1d58eb2c26d723
Chase Thomas <cj.thomas19@students.bchigh.edu>
2020-07-16 18:33:39 -0400

    Deorganize by Date
    Was affecting runtime in VSCode

---
+++ Greeter.java
@@ -0,0 +1,7 @@
+public class Greeter {
+    
+    public Greeter(String message) {
+        System.out.println(message);
+    }
+    
+}
---
+++ HelloClasses.java
@@ -0,0 +1,6 @@
+public class HelloClasses {
+    
+    public static void main(String[] args) {
+        Greeter greet = new Greeter("Hello, Classes!");
+    }
+}
