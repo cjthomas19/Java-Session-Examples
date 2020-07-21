2f1181af0b2f6c68ed1c5309f98d67403e15078c
Chase Thomas <cj.thomas19@students.bchigh.edu>
2020-07-21 16:40:40 -0400

    Changed name of scanner for consistency

--- Day2/P1_Input.java
+++ Day2/P1_Input.java
@@ -7,19 +7,19 @@ public class P1_Input {
     public static void main(String args[]) {
 
         //Initialize a variable, 'scan', as our scanner object
-        Scanner scan = new Scanner(System.in);
+        Scanner inputStream = new Scanner(System.in);
 
         //Prompt the user for input
         System.out.print("Write something: ");
 
         //Store the value of the scanner's input line as a string
-        String inString = scan.nextLine();
+        String inString = inputStream.nextLine();
 
         //Print the user's input back to them
         System.out.println("You wrote: " + inString);
 
         //Close scanner to prevent resource leaks
-        scan.close();
+        inputStream.close();
 
     }
 
